af4c62533b12481e758e124395c3b8532c87918a
enaguero <enaguero@gmail.com>
2024-12-16 16:07:09 +0000

    Code example done

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,6 +3,19 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+function generateDatabaseDateTime(date) {
+	return date.toISOString().replace("T"," ").substring(0, 19);
+}
+
+const callMe = (seconds) => {
+	alert("The function callMe is being executed!!! ")
+
+	setTimeout(() => {
+		let currentDateTime = new Date();
+		console.log("I am calling you !!!! at : " + generateDatabaseDateTime(currentDateTime));
+	},seconds)
+}
+
 //create your first component
 const Home = () => {
 	return (
@@ -13,6 +26,29 @@ const Home = () => {
 			<p>
 				<img src={rigoImage} />
 			</p>
+
+			<button onClick={ ()=> callMe(5000)}> Call me in 5 seconds</button><br/><br/>
+			<h1>Introduce el numero correcto cual llamar:</h1>
+			<input onChange={(evt)=>{
+				let target = evt.target;
+
+				let promiseToCallYou = new Promise((resolve, reject) => {
+					setTimeout(() => {
+						if(target.value === "666"){
+							console.log("I am calling you!!!")
+							resolve("The call has been done sucessfully");
+						}
+						else{
+							reject(new Error("The number is not correct"));
+						}
+					},2000)
+				})
+
+				promiseToCallYou.then((message) => {
+					console.log(message)
+				})
+				
+			} } placeholder="Please introduce correct number to call" /><br/><br/>
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working...
 			</a>
